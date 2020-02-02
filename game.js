f36f5edab4eaac5a6a4547903f9ee5c15956ffe2
zow one <jeremie@10mentionweb.org>
2020-02-02 23:52:33 +0100

    try to animate

--- game.js
+++ game.js
@@ -19,19 +19,35 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload ()
-{
+function preload (){
     this.load.image('ground', 'images/ground.png');
+    this.load.atlas('spaceship', 'images/space.png','images/space.json');
+
+    this.anims.create({
+        key:'powerShip',
+        frameRate:10,
+        frames: this.anims.generateFrameNames('spaceship',{
+            prefix:'power',
+            sufix:'.png',
+            start:1,
+            end:3,
+        }),
+        repeat:-1
+    })
 }
 
-function create ()
-{
+function create (){
     const level = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]];
     const map = this.make.tilemap({data:level,tileWidth:32,tileHeight:32});
     const tiles = map.addTilesetImage('ground');
-    const layer = map.createStaticLayer(0, tiles, 0, 736)
+    map.createStaticLayer(0, tiles, 0, 736);
+
+    
+    const mainSpaceship =  this.add.sprite(0,0,"spaceship", "spaceship");
+     const power  =  this.add.sprite(47,5,"spaceship").play('powerShip');
+
+     const spaceship__container = this.add.container(200,100,[mainSpaceship,power]);
 }
 
-function update ()
-{
+function update (){
 }
